a7931cd30075faa0d52ddd7083628793768b7ee5
Jeanette Gonzalez <jeanettegonzalezlovesu@gmail.com>
2020-03-14 19:39:41 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,5 +1,5 @@
 # Build your say_hello method here
-def say_hello(name = "Ruby Programmer")
-  puts "hello, #{name}!"
+def say_hello(name = "Ruby Programmer!")
+  puts "Hello #{name}!"
   say_hello("Kent Beck!")
 end
